9c50a03ec7c338d32d9597704b27cb3119434511
Aman Dwivedi <amandwivedi71124.com>
2023-07-12 14:03:01 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import CustomButton from "./CustomButton";
+export  {CustomButton}
+export {default as ColorPicker} from "./ColorPicker"
+export {default as AiPicker} from "./AiPicker"
+export {default as FilePicker} from "./FilePicker"
+export {default as Tab} from "./Tab"
